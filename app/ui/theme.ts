78cbdc9fd72d518900f46a0c69b286e7a5ea0324
zhangzhong <im.zhong@outlook.com>
2024-05-17 19:21:11 +0800

    add material ui template of login, register, about pages

---
+++ app/ui/theme.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+import { roboto } from "@/app/ui/font";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
