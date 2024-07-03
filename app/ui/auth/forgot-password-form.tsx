1409cb181b5ad6aea9b8be35fc642bad25eebfa1
zhangzhong <im.zhong@outlook.com>
2024-07-03 18:08:25 +0800

    refactor forgot password form

--- app/ui/auth/forgot-password-form.tsx
+++ app/ui/auth/forgot-password-form.tsx
@@ -3,22 +3,45 @@
 "use client";
 
 import * as React from "react";
-import { Button, TextField, Box, Typography } from "@mui/material";
+import {
+  Button,
+  Typography,
+  Stack,
+  FormHelperText,
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+  Alert,
+  Box,
+  TextField,
+} from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 import { Link } from "@mui/material";
 import { authClient } from "@/app/lib/auth/auth_client";
 import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface ForgotPasswordFormData {
-  email: string;
-}
+const schema = z.object({
+  email: z.string().min(1, { message: "email is required" }).email(),
+});
+
+type ForgotPasswordFormData = z.infer<typeof schema>;
 
 // 输入忘记密码之后应该干什么？
 // 应该等待后端发送邮件地址
 // 点击链接会进入一个重置密码的界面，只需要填入新密码并重复一次就ok啦
 // 所以我们还需要提供一个界面，reset-password-form!
 export default function ForgotPasswordForm() {
-  const { handleSubmit, control } = useForm<ForgotPasswordFormData>();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setError,
+  } = useForm<ForgotPasswordFormData>({
+    resolver: zodResolver(schema),
+  });
   const router = useRouter();
   // 这里和登录一样
   // TODO
@@ -32,39 +55,48 @@ export default function ForgotPasswordForm() {
     console.log(data);
     const { error } = await authClient.forgotPassword({ email: data.email });
     if (error) {
-      alert(error);
+      // alert(error);
+      setError("root", { message: error });
+      return;
     }
     setIsSendEmail(true);
     // router.push("/agent");
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: 2,
-      }}
-    >
-      <Controller
-        name="email"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField label="Email" variant="outlined" {...field} />
-        )}
-      />
-      <Button variant="contained" type="submit">
+    <Stack spacing={2}>
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
         Forgot Password
-      </Button>
+      </Typography>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <FormControl error={Boolean(errors.email)}>
+                <InputLabel>email</InputLabel>
+                <OutlinedInput {...field} label="email" type="email" />{" "}
+                {errors.email && (
+                  <FormHelperText>{errors.email.message}</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+          <Button variant="contained" type="submit">
+            Forgot Password
+          </Button>
+        </Stack>
+      </form>
+
       {isSendEmail && (
-        <Typography variant="subtitle1" sx={{ mb: 2 }}>
+        <Alert severity="success">
           We have sent a verify email to your email address, please check it.
-        </Typography>
+        </Alert>
       )}
-    </Box>
+
+      {errors.root && <Alert severity="warning">{errors.root.message}</Alert>}
+    </Stack>
   );
 }
