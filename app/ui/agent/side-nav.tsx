4e5d7912709263bd3dc16dc84c0511431e96af03
zhangzhong <im.zhong@outlook.com>
2024-07-07 13:35:02 +0800

    add nav bar footer

--- app/ui/agent/side-nav.tsx
+++ app/ui/agent/side-nav.tsx
@@ -24,6 +24,10 @@ import { useRouter } from "next/navigation";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 import TocIcon from "@mui/icons-material/Toc";
 import { usePathname } from "next/navigation";
+import { Stack, Paper } from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 const drawerWidth = 200;
 
@@ -66,6 +70,65 @@ function NavItem({
   );
 }
 
+function NavFooter() {
+  return (
+    <Stack direction="column" spacing={1}>
+      <Paper
+        sx={{
+          px: 2,
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          ":hover": {
+            backgroundColor: theme.palette.grey[200],
+            cursor: "pointer",
+          },
+        }}
+        onClick={() => {}}
+      >
+        <Stack direction="column">
+          <Stack alignItems="center">
+            <EmailIcon fontSize="small" />
+          </Stack>
+          <Stack alignItems="center">
+            <Typography variant="body2">im.zhong@outlook.com</Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+
+      <Paper
+        sx={{
+          px: 2,
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          ":hover": {
+            backgroundColor: theme.palette.grey[200],
+            cursor: "pointer",
+          },
+        }}
+        onClick={() => {
+          window.location.href = "https://github.com/im-zhong";
+        }}
+      >
+        <Stack direction="column">
+          <Stack alignItems="center">
+            <GitHubIcon fontSize="small" />
+          </Stack>
+
+          <Stack alignItems="center">
+            <Typography variant="body2">https://github.com/im-zhong</Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Stack>
+  );
+}
+
 export function LeftNav() {
   const pathname = usePathname();
   console.log("pathname", pathname);
@@ -87,26 +150,51 @@ export function LeftNav() {
         [`& .MuiDrawer-paper`]: {
           width: drawerWidth,
           boxSizing: "border-box",
+          flexDirection: "column",
         },
       }}
     >
       {
         // 原来如此，原来每个元素前面都有一个ToolBar占了一个位子
         // 实际上AppBar是不参与布局的 他就是固定的
+        // flex: 1非常关键，他会充满整个空间
+        // 把最后的box顶到最下面
+        /*
+        The flex: 1 CSS property in a Flexbox layout has a specific meaning. It is shorthand for three properties: flex-grow, flex-shrink, and flex-basis. When you use flex: 1, it is equivalent to flex-grow: 1; flex-shrink: 1; flex-basis: 0%. Here's what each of these properties means in this context:
+        
+        flex-grow: 1; This property defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up. If all items have flex-grow set to 1, the remaining space in the container will be distributed equally to all children.
+        flex-shrink: 1; This property defines the ability for a flex item to shrink if necessary. Like flex-grow, it accepts a unitless value that serves as a proportion. It dictates how much the flex item will shrink relative to the rest of the flex items in the container when there isn't enough space on the row.
+        flex-basis: 0%; This property defines the default size of an element before the remaining space is distributed. It can be a length (e.g., 20%, 5rem, etc.) or a keyword. The 0% value means that the size of items is initially set to zero, and they will only grow or shrink from that point based on the flex-grow and flex-shrink values.
+
+        So, when you apply flex: 1 to an element in a Flexbox layout, you're essentially telling it to take up as much space as it can, proportionally to its flex siblings, after accounting for any items that do not have flexibility (flex-grow: 0), and to shrink if necessary, starting from a base size of zero. This is a common pattern used to make elements fill up the remaining space in a container.
+        */
       }
+      <Box
+        sx={{
+          flex: 1,
+          // border: "1px solid black"
+        }}
+      >
+        {" "}
+        {/* This Box wraps the List and allows it to grow */}
+        <List>
+          <NavItem icon={<HomeIcon />} text="Home" href="/agent" />
+          <NavItem icon={<HomeIcon />} text="Home" href="/agent" />
 
-      <List>
-        <NavItem icon={<HomeIcon />} text="Home" href="/agent" />
-        <NavItem icon={<HomeIcon />} text="Home" href="/agent" />
+          <Divider />
 
-        <Divider />
+          <NavItem icon={<SmartToyIcon />} text="Create" href="/agent/create" />
+          <Divider />
 
-        <NavItem icon={<SmartToyIcon />} text="Create" href="/agent/create" />
-        <Divider />
+          <NavItem icon={<TocIcon />} text="Chats" href="/agent/my-chats" />
+          <Divider />
+        </List>
+      </Box>
 
-        <NavItem icon={<TocIcon />} text="Chats" href="/agent/my-chats" />
-        <Divider />
-      </List>
+      {/* Logout Button at the end */}
+      <Box sx={{ padding: 2 }}>
+        <NavFooter />
+      </Box>
     </Drawer>
   );
 }
