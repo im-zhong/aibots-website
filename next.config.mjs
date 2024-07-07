a281f9dfa8a91d600cdff2e59edb6e34cec693ce
zhangzhong <im.zhong@outlook.com>
2024-07-07 15:31:55 +0800

    do not know how to build without test page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  swcMinify: false, // it should be false by default
+  swcMinify: false,
+  // isProduction: process.env.NODE_ENV === "production",
 };
 
 export default nextConfig;
